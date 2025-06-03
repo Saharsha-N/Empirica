792c28ae33bc060df960a8b8fa08199a46d15406
Saharsha Navani <vansah2017@gmail.com>
2025-06-03 09:50:15 -0400

    Initialize suggestions package

---
+++ empirica/suggestions/__init__.py
@@ -0,0 +1,14 @@
+"""
+Intelligent research suggestions module.
+"""
+
+from .engine import SuggestionEngine
+from .integration import SuggestionIntegration
+from .display import SuggestionDisplay
+
+__all__ = [
+    'SuggestionEngine',
+    'SuggestionIntegration',
+    'SuggestionDisplay',
+]
+
