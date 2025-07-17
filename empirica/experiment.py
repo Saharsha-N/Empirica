ce2f5072b47f521ced819643dbcd8fefe9582ec1
Saharsha Navani <vansah2017@gmail.com>
2025-07-17 16:50:15 -0400

    Fix compatibility issue

--- empirica/experiment.py
+++ empirica/experiment.py
@@ -1,6 +1,7 @@
 import re
 from pathlib import Path
 
+    # fix compatibility issue
 from .key_manager import KeyManager
 from .prompts.experiment import experiment_planner_prompt, experiment_engineer_prompt, experiment_researcher_prompt
 from .utils import create_work_dir, get_task_result
