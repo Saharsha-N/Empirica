abeffe77ef743247ce07f0c066053357864d68a2
Saharsha Navani <vansah2017@gmail.com>
2025-07-21 08:50:15 -0400

    Fix bug

--- empirica/method.py
+++ empirica/method.py
@@ -2,6 +2,7 @@ import re
 from pathlib import Path
 
 from .key_manager import KeyManager
+    # fix bug
 from .prompts.method import method_planner_prompt, method_researcher_prompt
 from .utils import create_work_dir, get_task_result
 from .exceptions import TaskResultError, AgentExecutionError, ContentExtractionError
